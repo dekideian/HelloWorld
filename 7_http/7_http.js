ded761a525c972832881dcd7dcc681334551b383
Deian <deki.deian@gmail.com>
2016-05-13 00:14:20 +0300

    7 & 9 updates
    these are the correct ways

--- 7_http/7_http.js
+++ 7_http/7_http.js
@@ -1,6 +1,6 @@
 var http = require('http')
 var url = "www.google.com" //process.argv[2]
-console.log("process.argv:"+process.argv[2])
+//console.log("process.argv:"+process.argv[2])
 http.get(process.argv[2], function(response){
 	response.on("data", function(data){
 		console.log(data)
--- 9_async/9_async.js
+++ 9_async/9_async.js
@@ -1,8 +1,48 @@
 var http = require('http')
-var bl= require('bl')
+var bl = require('bl')
+var result = []
+ 
+function print(){
+	for(var i =0;i<3;i++)
+		console.log(result[i])	
+} 
+function getHttp(index){
+	http.get(process.argv[2+index], function(response){
+		response.pipe(bl(function(err,data){		
+			if(err)
+				console.err("error")
+			else{
+				result[index] = data.toString()
+				if(index==2)
+					print()				
+			}
+		}))
+	})	
+}
+for (var i = 0;i<3;i++)
+	getHttp(i)
+
+
+/* old approach
+var bl2= require('bl')
+var bl3= require('bl')
+var bl4= require('bl')
+
+
 http.get(process.argv[2], function(response){	
-	response.pipe(bl(function(err,data){
-		console.log(data.toString().length)
+	response.pipe(bl2(function(err,data){		
 			console.log(data.toString())			
 		}))
 })
+http.get(process.argv[3], function(response){	
+	response.pipe(bl3(function(err,data){		
+			console.log(data.toString())			
+		}))
+})
+http.get(process.argv[4], function(response){	
+	response.pipe(bl4(function(err,data){		
+			console.log(data.toString())			
+		}))
+})
+*/
+
